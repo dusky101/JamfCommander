647ae7b7e967df21a9cbf866f6a275e2b7bcb759
Marc Oliff <marc.oliff@outlook.com>
2026-01-16 23:32:10 +0000

    Initial Commit

---
+++ JamfCommander/JamfCommanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamfCommanderApp.swift
+//  JamfCommander
+//
+//  Created by Marc Oliff on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct JamfCommanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
